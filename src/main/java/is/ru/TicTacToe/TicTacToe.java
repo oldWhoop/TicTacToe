4c3e6ea277c881e529b81a38243e64e8f1c8cece
7oi <sevenoi@sevenoi.com>
2013-11-14 21:24:54 +0000

    Adapted more from the demo project to get this running

--- src/main/java/is/ru/TicTacToe/TicTacToe.java
+++ src/main/java/is/ru/TicTacToe/TicTacToe.java
@@ -1,13 +1,21 @@
 package is.ru.TicTacToe;
 
-/**
- * Hello world!
- *
- */
-public class TicTacToe 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import static spark.Spark.*;
+import spark.*;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        
+        setPort(Integer.valueOf(System.getenv("PORT")));
+
+        post(new Route("/add") {
+            @Override
+            public Object handle(Request request, Response response) {
+                Integer a = Integer.valueOf(request.queryParams("a"));
+                Integer b = Integer.valueOf(request.queryParams("b"));
+                return a + b;
+            }
+        });
     }
 }

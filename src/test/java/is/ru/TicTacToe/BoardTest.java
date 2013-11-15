51c36ebc68e61092814e363c15960b16c416be19
7oi <sevenoi@sevenoi.com>
2013-11-15 13:37:19 +0000

    Added BoardTest.java

---
+++ src/test/java/is/ru/TicTacToe/BoardTest.java
@@ -0,0 +1,28 @@
+package is.ru.TicTacToe;
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.Test;
+
+
+public class BoardTest {
+	Board b = new Board();
+
+    @Test
+    public void testBoardArray() {
+    	int counter = 0;
+    	for (int i = 0; i < 9; i++) {
+    		assertEquals(0, b.get(i));
+    	}
+    }
+
+    @Test
+    public void testSetGet() {
+    	int n = 4;
+    	int val = 3;
+    	b.set(n, val);
+    	assertEquals(val, b.get(n));
+    }
+
+
+}

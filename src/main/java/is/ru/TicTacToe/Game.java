c47590cb06a577671905d2e869089d0ed16c5741
7oi <sevenoi@sevenoi.com>
2013-11-15 15:05:22 +0000

    Added player var to class and tests

--- src/main/java/is/ru/TicTacToe/Game.java
+++ src/main/java/is/ru/TicTacToe/Game.java
@@ -5,28 +5,41 @@ public class Game {
 
 	public Board gameBoard;
 	private int round = 0;
+	private int player = 1;
+	private int MAXROUND = 9;
 
+	// Constructor
 	public Game() {
 		gameBoard = new Board();
-	}
-
-	public void mark(int n, int val) {
-		gameBoard.set(n, val);
-		nextRound();
 	}
 
 	public int getRound() {
 		return round;
 	}
 
-	public void nextRound() {
-		round++;
-	}
-
+	// New round, clears the board
 	public void newRound() {
 		round = 0;
 		gameBoard.clearBoard();
 	}
 
+	// Advance the game
+	public void nextRound() {
+		round++;
+		if (round == MAXROUND) {
+			round = 0;
+		}
+		player = round%2 + 1;
+	}
 
+	// Get player number
+	public int getPlayer() {
+		return player;
+	}
+
+	// Mark square for player
+	public void mark(int n, int val) {
+		gameBoard.set(n, val);
+		nextRound();
+	}
 }
--- src/test/java/is/ru/TicTacToe/GameTest.java
+++ src/test/java/is/ru/TicTacToe/GameTest.java
@@ -14,12 +14,42 @@ public class GameTest {
     		assertEquals(0, g.gameBoard.get(i));
     	}
     	assertEquals(0, g.getRound());
+    	assertEquals(1, g.getPlayer());
 	}
 
 	@Test
 	public void testNextRound() {
 		g.nextRound();
 		assertEquals(1, g.getRound());
+		assertEquals(2, g.getPlayer());
+	}
+
+	@Test
+	public void testMark() {
+		int square = 4;
+		int val = 2;
+		g.mark(square, val);
+		assertEquals(val, g.gameBoard.get(square));
+		assertEquals(1, g.getRound());
+		assertEquals(2, g.getPlayer());
+	}
+
+	@Test
+	public void testNewRound() {
+		for (int i = 0; i < 9; i++) {
+			g.mark(i, (i%2)+1);
+		}
+		g.newRound();
+		testGameBoard();
+	}
+
+	@Test
+	public void testMaxRound() {
+		for(int i = 0; i < 9; i++) {
+			g.mark((i), (i%2)+1);
+		}
+		assertEquals(0, g.getRound());
+		assertEquals(1, g.getPlayer());
 	}
 
 

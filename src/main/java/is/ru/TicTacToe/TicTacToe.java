1b6bde6303b9583199268ba1107f7efbcc1e40ff
Eyjolfur Kristjansson <eyjolfur12@ru.is>
2013-11-19 10:13:27 +0000

    Game create in TicTacToe class. Return winner

--- src/main/java/is/ru/TicTacToe/TicTacToe.java
+++ src/main/java/is/ru/TicTacToe/TicTacToe.java
@@ -9,12 +9,15 @@ public class TicTacToe {
         
         setPort(Integer.valueOf(System.getenv("PORT")));
 
+        final Game g = new Game();
+
         post(new Route("/add") {
             @Override
             public Object handle(Request request, Response response) {
-                Integer a = Integer.valueOf(request.queryParams("a"));
-                Integer b = Integer.valueOf(request.queryParams("b"));
-                return a + b;
+                Integer val = Integer.valueOf(request.queryParams("val"));
+                int ret = g.oneTurn(val);
+
+                return ret;
             }
         });
     }
